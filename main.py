eee6a9de12ed6d692b7c7501507ebd46b21617d0
nitropython32-sys <nitropython32@gmail.com>
2025-10-18 23:05:06 -0700

    update json output

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-# import moondream as md
-# from PIL import Image
-# from config import API_KEY
-
-# # connect to Moondream Station
-# # Initialize for Moondream Cloud
-# model = md.vl(api_key=API_KEY)
-
-# # Load an image
-# image = Image.open("frame_135s.jpg")
-
-
-# # Ask a question
-# answer = model.query(image, """inside this image is a UFC fight, I need you to find the time clock. inside the time clock its should 
-#                      the time of the round and what round `it is. rounds are between 1-3 or 1-5 depending on if its a championship fight or not.
-#                      something to remember is sometimes you wont see a time clock at all and sometimes the fight the will be in between
-#                      rounds as well. if thats the case return a null in json format else
-#                      Return the time and rnd in json format""")["answer"]
-# print("Answer:", answer)
-
-
 import moondream as md
 from PIL import Image
 import json
@@ -33,16 +12,24 @@ v = Video("/home/nigel/Desktop/Projects/UFCRoundSplit/Fights/Yair Rodriguez vs T
 indexes = Video.get_frame_indices(v.total_len, 8)
 frames = [v.extract_frame(i / v.fps) for i in indexes]
 
-# feed frames into model and collect captions
 results = []
-for frame in frames:
-    img = Image.fromarray(frame)
+for i in indexes:
+    timestamp = i / v.fps
+    img = Image.fromarray(frames[indexes.index(i)])
+    
     answer = model.query(img, """inside this image is a UFC fight, I need you to find the time clock. inside the time clock its should 
                      the time of the round and what round `it is. rounds are between 1-3 or 1-5 depending on if its a championship fight or not.
                      something to remember is sometimes you wont see a time clock at all and sometimes the fight the will be in between
                      rounds as well. if thats the case return a null in json format else
                      Return the time and rnd in json format""")["answer"]
-    results.append(answer)
+
+    results.append({
+        "frame_index": i,
+        "video_time": Video.format_time(timestamp),
+        "raw_time_seconds": timestamp,
+        "model_response": answer
+    })
+
 
 # save all 8 responses in one JSON file
 with open("results.json", "w") as f:
